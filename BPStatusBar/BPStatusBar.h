2f491f0dcdbf656a4cb33177082357b87450ffed
Brian Partridge <brianpartridge@gmail.com>
2013-04-09 00:47:19 -0400

    initial version
    - Support for spinners, images, and text.
    - Include success and error images from Glyphish in a bundle.

---
+++ BPStatusBar/BPStatusBar.h
@@ -0,0 +1,71 @@
+//
+//  BPStatusBar.h
+//  BPStatusBar
+//
+//  Created by Brian Partridge on 4/8/13.
+//  Copyright (c) 2013 Brian Partridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AvailabilityMacros.h>
+
+@interface BPStatusBar : UIView
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 50000
+@property (readwrite, nonatomic, retain) UIColor *foregroundColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
+@property (readwrite, nonatomic, retain) UIFont *font NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
+#endif
+
+/**
+ * Access the animation used to transition to/from the system status bar.
+ * @default UIStatusBarAnimationFade
+ */
++ (UIStatusBarAnimation)transitionStyle;
+
+/**
+ * Set the animation used to transition to/from the system status bar.
+ */
++ (void)setTransitionStyle:(UIStatusBarAnimation)transitionStyle;
+
+/**
+ * Show spinner with status string.
+ */
++ (void)showWithStatus:(NSString*)status;
+
+/**
+ * Show determinate progress indicator with status string.
+ */
++ (void)showProgress:(CGFloat)progress status:(NSString*)status;
+
+/**
+ * Change the status while already showing.
+ */
++ (void)setStatus:(NSString*)status;
+
+/**
+ * Stops the activity indicator, shows a glyph and status, and dismisses 1 second later.
+ */
++ (void)showSuccessWithStatus:(NSString*)status;
+
+/**
+ * Stops the activity indicator, shows a glyph and status, and dismisses 1 second later.
+ */
++ (void)showErrorWithStatus:(NSString *)status;
+
+/**
+ * Stops the activity indicator, shows a glyph and status, and dismisses 1 second later.
+ * @param image Image to display. Should be a 14x14 white png.
+ */
++ (void)showImage:(UIImage*)image status:(NSString*)status;
+
+/**
+ * Dismiss the BPStatusBar and restore the system status bar.
+ */
++ (void)dismiss;
+
+/**
+ * Indicates whether the BPStatusBar is visible.
+ */
++ (BOOL)isVisible;
+
+@end
---
+++ BPStatusBar/BPStatusBar.m
@@ -0,0 +1,266 @@
+//
+//  BPStatusBar.m
+//  BPStatusBar
+//
+//  Created by Brian Partridge on 4/8/13.
+//  Copyright (c) 2013 Brian Partridge. All rights reserved.
+//
+
+#import "BPStatusBar.h"
+
+#define ACCESSORY_DIMENSION 14.0
+#define HORIZONTAL_PADDING 10.0
+#define STATUSBAR_TRANSITION_DURATION 0.3
+
+@interface BPStatusBar ()
+
+@property (nonatomic, strong) UILabel *statusLabel;
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIActivityIndicatorView *spinner;
+
+@end
+
+@implementation BPStatusBar
+
+static UIStatusBarAnimation _transitionStyle;
+
++ (void)initialize {
+    _transitionStyle = UIStatusBarAnimationFade;
+}
+
+#pragma mark - Class Property
+
++ (UIStatusBarAnimation)transitionStyle {
+    return _transitionStyle;
+}
+
++ (void)setTransitionStyle:(UIStatusBarAnimation)transitionStyle {
+    _transitionStyle = transitionStyle;
+}
+
+#pragma mark - Singleton
+
++ (BPStatusBar *)sharedView {
+    static BPStatusBar *sharedView;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        UIApplication *app = [UIApplication sharedApplication];
+        sharedView = [[BPStatusBar alloc] initWithFrame:CGRectMake(0, 0, app.statusBarFrame.size.width, app.statusBarFrame.size.height)];
+    });
+    return sharedView;
+}
+
+#pragma mark - Show Methods
+
++ (void)showWithStatus:(NSString*)status {
+    [[self sharedView] showStatus:status transitionStyle:[self transitionStyle]];
+}
+
++ (void)showProgress:(CGFloat)progress status:(NSString*)status {
+    [[self sharedView] showStatus:status transitionStyle:[self transitionStyle]];
+}
+
++ (void)setStatus:(NSString *)status {
+    [[self sharedView] setStatus:status];
+}
+
+#pragma mark - Show Then Dismiss Methods
+
++ (void)showSuccessWithStatus:(NSString*)status {
+    [self showImage:[UIImage imageNamed:@"BPStatusBar.bundle/success.png"] status:status];
+}
+
++ (void)showErrorWithStatus:(NSString *)status {
+    [self showImage:[UIImage imageNamed:@"BPStatusBar.bundle/error.png"] status:status];
+}
+
++ (void)showImage:(UIImage*)image status:(NSString*)status {
+    [[self sharedView] showImage:image status:status transitionStyle:[self transitionStyle]];
+}
+
+#pragma mark - Visibility Methods
+
++ (void)dismiss {
+    [[self sharedView] dismiss:[self transitionStyle]];
+}
+
++ (BOOL)isVisible {
+    return ([UIApplication sharedApplication].statusBarHidden &&
+            [self sharedView].superview != nil);
+}
+
+#pragma mark - Lifecycle
+
+- (id)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	if (!self) {
+        return nil;
+	}
+
+    self.userInteractionEnabled = NO;
+    self.backgroundColor = [UIColor blackColor];
+    self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+
+	return self;
+}
+
+- (void)layoutSubviews {
+    CGRect availableBounds = CGRectInset(self.bounds, HORIZONTAL_PADDING, 0);
+    availableBounds.size.width -= ACCESSORY_DIMENSION;
+
+    CGSize labelSize = [self.statusLabel sizeThatFits:availableBounds.size];
+
+    CGFloat totalContentWidth = ACCESSORY_DIMENSION + HORIZONTAL_PADDING + labelSize.width;
+    CGFloat hOffset = floorf((self.bounds.size.width - totalContentWidth) / 2);
+    CGRect accessoryFrame = CGRectMake(hOffset,
+                                       floorf((self.bounds.size.height - ACCESSORY_DIMENSION) / 2),
+                                       ACCESSORY_DIMENSION,
+                                       ACCESSORY_DIMENSION);
+    self.imageView.frame = self.spinner.frame = accessoryFrame;
+
+    self.statusLabel.frame = CGRectMake(CGRectGetMaxX(accessoryFrame) + HORIZONTAL_PADDING,
+                                        floorf((self.bounds.size.height - labelSize.height) / 2),
+                                        labelSize.width,
+                                        labelSize.height);
+}
+
+- (void)setStatus:(NSString *)status {
+    self.statusLabel.text = status;
+    [self setNeedsLayout];
+}
+
+#pragma mark - Show and Dismiss
+
+- (void)showStatus:(NSString *)status transitionStyle:(UIStatusBarAnimation)transitionStyle {
+    self.imageView.image = nil;
+    self.imageView.hidden = YES;
+    self.statusLabel.text = status;
+    [self.spinner startAnimating];
+
+    if (![[self class] isVisible]) {
+        self.frame = [UIApplication sharedApplication].statusBarFrame;
+        [self setNeedsLayout];
+        [[UIApplication sharedApplication].keyWindow addSubview:self];
+        [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:transitionStyle];
+    }
+}
+
+- (void)showImage:(UIImage *)image status:(NSString *)status transitionStyle:(UIStatusBarAnimation)transitionStyle {
+    self.imageView.image = image;
+    self.imageView.hidden = NO;
+    self.statusLabel.text = status;
+    [self.spinner stopAnimating];
+
+    if (![[self class] isVisible]) {
+        self.frame = [UIApplication sharedApplication].statusBarFrame;
+        [self setNeedsLayout];
+        [[UIApplication sharedApplication].keyWindow addSubview:self];
+        [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:transitionStyle];
+    }
+
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self dismiss:transitionStyle];
+    });
+}
+
+- (void)dismiss:(UIStatusBarAnimation)transitionStyle {
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:transitionStyle];
+
+    // TODO: This is a timing hack, it would be nice if we could get notified when the system status bar fully appears, but we can't currently.
+    double delayInSeconds = STATUSBAR_TRANSITION_DURATION;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self removeFromSuperview];
+    });
+}
+
+#pragma mark - Subviews
+
+- (UILabel *)statusLabel {
+    if (_statusLabel == nil) {
+        _statusLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _statusLabel.backgroundColor = [UIColor clearColor];
+        _statusLabel.adjustsFontSizeToFitWidth = YES;
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < 60000
+        _statusLabel.textAlignment = UITextAlignmentCenter;
+#else
+        _statusLabel.textAlignment = NSTextAlignmentCenter;
+#endif
+		_statusLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+
+        // UIAppearance is used when iOS >= 5.0
+		_statusLabel.textColor = self.foregroundColor;
+		_statusLabel.font = self.font;
+    }
+
+    if (_statusLabel.superview == nil) {
+        [self addSubview:_statusLabel];
+    }
+
+    return _statusLabel;
+}
+
+- (UIImageView *)imageView {
+    if (_imageView == nil) {
+        _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, ACCESSORY_DIMENSION, ACCESSORY_DIMENSION)];
+    }
+
+    if(_imageView.superview == nil) {
+        [self addSubview:_imageView];
+    }
+
+    return _imageView;
+}
+
+- (UIActivityIndicatorView *)spinner {
+    if (_spinner == nil) {
+        _spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+		_spinner.hidesWhenStopped = YES;
+
+        CGFloat scale = ACCESSORY_DIMENSION / _spinner.bounds.size.width;
+        _spinner.transform = CGAffineTransformMakeScale(scale, scale);
+
+        // UIAppearance is used when iOS >= 5.0
+        _spinner.color = self.foregroundColor;
+    }
+
+    if(_spinner.superview == nil) {
+        [self addSubview:_spinner];
+    }
+
+    return _spinner;
+}
+
+#pragma mark - UIAppearance Getters
+
+- (UIColor *)foregroundColor {
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 50000
+    if (_foregroundColor == nil) {
+        _foregroundColor = [[[self class] appearance] foregroundColor];
+    }
+
+    if (_foregroundColor != nil) {
+        return _foregroundColor;
+    }
+#endif
+
+    return [UIColor whiteColor];
+}
+
+- (UIFont *)font {
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 50000
+    if (_font == nil) {
+        _font = [[[self class] appearance] font];
+    }
+
+    if (_font != nil) {
+        return _font;
+    }
+#endif
+
+    return [UIFont boldSystemFontOfSize:14];
+}
+
+@end
